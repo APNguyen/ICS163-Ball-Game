44336ca09952962a61914fbba0502a93cdafd186
APNguyen <gr2yknight@yahoo.com>
2018-10-17 12:34:14 -0700

    Basic mechanics in place, prototype finished. Moving on to next game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = new Vector3(0, ball.transform.position.y + 2, ball.transform.position.z - .5f);
+        transform.position = pos;
+	}
+}
